63d11ffdaae8fc901a1cf2c42181a154f4725f85
muralithelearner <muraliprasad142@gmail.com>
2024-10-30 18:26:22 +0530

    Updated requirements for Cloudinary

--- hosteldb20/settings.py
+++ hosteldb20/settings.py
@@ -120,16 +120,17 @@ environ.Env.read_env()
 
 CLOUDINARY_URL = env('CLOUDINARY_URL')  # Load from .env file or environment variables
 
-
-
 # Cloudinary configuration
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
-    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
-}
+# CLOUDINARY_STORAGE = {
+#     'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
+#     'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
+#     'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
+# }
+CLOUDINARY_STORAGE = { 'folder': 'property_images' }
 
-# DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 
 
 
