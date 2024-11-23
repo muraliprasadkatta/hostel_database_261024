b6bbf08dad7d3b29cf9fb4226c794e96396d557d
muralithelearner <muraliprasad142@gmail.com>
2024-11-23 19:28:45 +0530

    Ad testing tempmate

--- hostelapp20/views.py
+++ hostelapp20/views.py
@@ -931,7 +931,9 @@ def ChangePassword(request, token):
 
         if new_password != confirm_password:
             messages.error(request, 'Passwords do not match.')
-            return render(request, 'registration/change_password.html', {'token': token})
+            # return render(request, 'registration/change_password.html', {'token': token})
+            return render(request, 'registration/testing_template.html', {'token': token})
+
 
         # Update the user's password
         user.set_password(new_password)
@@ -947,7 +949,8 @@ def ChangePassword(request, token):
         # Redirect to registration page with a flag
         return redirect(reverse('login_and_registration') + '?reset_success=true')
 
-    return render(request, 'registration/change_password.html', {'token': token})
+    # return render(request, 'registration/change_password.html', {'token': token})
+    return render(request, 'registration/testing_template.html', {'token': token})
 
 
 def Payments(request, property_id):
