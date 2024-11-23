7f057b19dce6645a8fc3809ee1475905d1ff3583
muralithelearner <muraliprasad142@gmail.com>
2024-11-23 11:30:15 +0530

    update the reg template and dashboard and forgot logic configuration

--- hosteldb20/settings.py
+++ hosteldb20/settings.py
@@ -42,16 +42,16 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # Database Configuration
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ.get('DB_NAME'),
+#         'USER': os.environ.get('DB_USER'),
+#         'PASSWORD': os.environ.get('DB_PASSWORD'),
+#         'HOST': os.environ.get('DB_HOST'),
+#         'PORT': os.environ.get('DB_PORT'),
+#     }
+# }
 
 # this is not a mandatory for local prodution
 
